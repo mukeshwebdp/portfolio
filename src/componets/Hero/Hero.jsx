780776c872276738f5c23c05bd1f3b28b157f516
mukeshwebdp <mukesh.webdp@gmail.com>
2024-06-03 13:24:40 +0530

    h

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Mukesh</h1>
         <p className={styles.description}>
-            I'm a full-stack developer with 5 years of exprience NodeJS. Reach out if you'd like to learn more!
+        Passionate about MongoDB, Express, React, and Node.js. Excited to start my career. Let's connect!
         </p>
         <a href="https://drive.usercontent.google.com/u/0/uc?id=1NCUDywaNuUBUm6wN8BS9yqCsyVfewV5j&export=download" className={styles.contactBtn}>Resume</a>
         </div>
